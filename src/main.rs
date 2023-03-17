f5acc6e2c9a5d646565eb88340be11e769dc8e03
Ehitale Ojiefoh <ehitale.ojiefoh.269@k12.friscoisd.org>
2023-03-17 04:02:02 +0000

    updated '.gitignore', misc.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
 
+const FIELD_WIDTH: usize = 12;
+const FIELD_HEIGHT: usize = 18;
+
 fn main() {
+    
     let mut tetromino: Vec<String> = vec![
         String::new(), 
         String::new(), 
@@ -19,7 +23,32 @@ fn main() {
     tetromino[4].push_str(".X...XX...X.....");
     tetromino[5].push_str(".X...X...XX.....");
     tetromino[6].push_str("..X...X..XX.....");
-    
 
+    //couldn't I just use a 'char' array? what do I need the Option for?
+    let mut p_field: [Option<char>; FIELD_WIDTH * FIELD_HEIGHT] = [None; FIELD_WIDTH * FIELD_HEIGHT];
+
+    for x in 0..FIELD_WIDTH {
+        for y in 0..FIELD_HEIGHT {
+            p_field[y * FIELD_WIDTH + x] = 
+            
+            if x == 0 || x == FIELD_WIDTH - 1 || y == FIELD_HEIGHT {
+                Some('9')
+            }
+
+            else {
+                None
+            }
+        }
+    }
+    
+    fn _rotate (px: u32, py: u32, r: u32) -> Option<u32> {
+        match r % 4 {
+            0 => Some(py * 4 + px),
+            1 => Some(12 + py - (px * 4)),
+            2 => Some(15 - (py * 4) - px),
+            3 => Some(3 - py + (px * 4)),
+            _ => None,    
+        }
+    }
     
 }
