3055c574ef30edeb2aa8926092970ecc3ff6ccd1
Ehitale Ojiefoh <ehiojiefoh2005@gmail.com>
2023-03-24 23:54:39 -0500

    made a cyan rectangle.

--- src/experiment.rs
+++ src/experiment.rs
@@ -1,11 +1,21 @@
-use std::io::{Write, stdout, Stdout};
-use crossterm::{ExecutableCommand, Result};
-use crossterm::{terminal::*, style::*};
+use std::io::{Write, stdout};
+use crossterm::{ExecutableCommand, QueueableCommand, Result};
+use crossterm::{terminal, style::{self, Stylize}, cursor};
 
 //function currently returns '()'
 pub fn clear_everything () -> Result<()> {
     let mut stdout = stdout();
-    stdout.execute(Clear(ClearType::All))?.execute(SetBackgroundColor(Color::Blue));
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
 
+    for y in 0..30 {
+        for x in 0..50 {
+            if (y == 0 || y == 30 - 1) || (x == 0 || x == 50 - 1) {
+                stdout
+                .execute(cursor::MoveTo(x, y))?
+                .execute(style::PrintStyledContent(" ".on(style::Color::Cyan)))?;
+            }
+        }
+    }
+    // stdout.flush()?;
     Ok(())
 }
