571fb564fca95e1199c74e28300254900cfbb19e
ltexensis <ehitale.ojiefoh.269@k12.friscoisd.org>
2023-04-05 04:12:20 +0000

    printed first tetromino.

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,11 @@ fn main() -> Result<()> {
     
     buffer.execute(terminal::Clear(terminal::ClearType::All))?;
     buffer.execute(terminal::SetSize(terminal::size()?.0, terminal::size()?.1))?;            
+
+    let current_piece = 2;
+    let current_rotation = 0;
+    let current_x = FIELD_WIDTH as u8 / 2;
+    let current_y = 0;
     
     let game_over = false;
 
@@ -77,7 +82,7 @@ fn main() -> Result<()> {
                 buffer.write(
                     // this is an expression that returns a reference to a slice of u8, which indexed by an indexed value of p_field.
                     &[" ABCDEFG=#".as_bytes()[p_field[y * FIELD_WIDTH + x]]]
-                ).unwrap();
+                );
     
                 thread::sleep(time::Duration::from_millis(50));
             }
@@ -86,7 +91,10 @@ fn main() -> Result<()> {
         // draw current piece
         for px in 0..4 {
             for py in 0..4 {
-                
+                if tetromino[current_piece as usize].as_bytes()[rotate(px, py, current_rotation) as usize] == b'X' {
+                    buffer.execute(cursor::MoveTo((current_y + px) as u16, (current_x + py) as u16))?;
+                    buffer.write(&[current_piece + 65]);
+                }
             }
         }
     }
@@ -98,6 +106,7 @@ fn main() -> Result<()> {
     
 }
 
+// remember to pass tetro and p_field!!!!
 fn does_tetromino_fit (tetro: [&str; 7], p_field: [usize; FIELD_WIDTH & FIELD_HEIGHT], id_tetromino: u8, rotation: u8, x_pos: u8, y_pos: u8) -> bool {
     for px in 0..4 {
         for py in 0..4 {
@@ -107,8 +116,8 @@ fn does_tetromino_fit (tetro: [&str; 7], p_field: [usize; FIELD_WIDTH & FIELD_HE
             // gets index into field
             let fi: u8 = (y_pos + py) * FIELD_WIDTH as u8 + (x_pos + px);
             
-            if x_pos + px >= 0 && x_pos + px < 12 {
-                if y_pos + py >= 0 && y_pos + py < 18 {
+            if x_pos + px >= 0 && x_pos + px < FIELD_WIDTH as u8 {
+                if y_pos + py >= 0 && y_pos + py < FIELD_HEIGHT as u8 {
                     if tetro[id_tetromino as usize].as_bytes()[pi as usize] == b'X' && p_field[fi as usize] != 0 {
                         return false;
                     }
