80b93fffa1353934f3fb0059ab86bf4f540c525a
ltexensis <ehitale.ojiefoh.269@k12.friscoisd.org>
2023-04-27 04:08:53 +0000

    lots of progress on no library version

--- src/experiment.rs
+++ src/experiment.rs
@@ -1,25 +1,107 @@
-use std::io::{stdout};
+use std::io::{stdout, Write};
 use crossterm::{ExecutableCommand, Result};
 use crossterm::{terminal, style::{self, Stylize}, cursor};
+use crossterm::event::{read, Event};
 
-pub fn _clear_everything () -> Result<()> {
+// use std::{thread, time
+
+const FIELD_WIDTH: u8 = 12;
+const FIELD_HEIGHT: u8 = 12;
+
+pub fn _alternate_screen () -> Result<()> {
     let mut stdout = stdout();
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
-    
-    let size = match terminal::size() {
-        Ok((x, y)) => (x, y),
-        Err(_) => panic!("couldn't find terminal dimensions"),
-    };
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?.execute(terminal::EnterAlternateScreen)?;
 
-    for y in 0..size.1 {
-        for x in 0..size.0 {
-            if (y == 0 || y == size.1 - 1) || (x == 0 || x == size.0 - 1) {
-                stdout
-                .execute(cursor::MoveTo(x, y))?
-                .execute(style::PrintStyledContent("a".cyan()))?;
-            }
-        }
-    }
+    // thread::sleep(time::Duration::from_millis(3000));
+
+    stdout.write(b"Can you see me?")?;
+
+    // thread::sleep(time::Duration::from_millis(3000));
+    
+    stdout.execute(terminal::LeaveAlternateScreen)?;
     // stdout.flush()?;
     Ok(())
 }
+
+pub fn _keyboard_events () -> Result<()> {
+    let mut stdout = stdout();
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    terminal::enable_raw_mode().unwrap();
+    
+    loop {
+        match read()? {
+            Event::Key(event) => println!("{:?}", event.code), 
+            _ => (),
+        }
+    }
+
+    Ok(())
+}
+
+pub fn no_library () -> Result<()> {
+    let mut p_field: &mut [u8] = &mut [0; 144];
+    
+    for x in 0..12 {
+        for y in 0..12 {
+            p_field[(y * 12 + x) as usize] = 
+            if x == 0 || x == 11 - 1 || y == 11 - 1 {
+                9
+            } else {
+                0
+            }
+        }
+    }
+    
+    // let mut screen_buffer: &mut [u8] = &mut [65; 144];
+    let mut stdout = stdout();
+    
+    for x in 0..12 {
+        for y in 0..12 {
+            stdout.write(
+                &[" ABCDEFG=#".as_bytes()[p_field[(y * 12 + x) as usize] as usize]]
+            )?;
+        }
+    }
+    
+    // stdout.write(screen_buffer)?;
+
+    // questions to ask, what does flush do and why do I need to do it?
+    // how do I implement cursor movement/can anyone explain this code? show crossterm cursor movement code.
+    Ok(())
+}
+
+pub fn no_library_extended () -> Result<()> {
+let p_field: &mut [u8] = &mut [0; 144];
+    
+    for x in 0..FIELD_WIDTH {
+        for y in 0..FIELD_HEIGHT {
+            p_field[(y * FIELD_WIDTH + x) as usize] = 
+            if y == 0 || x == FIELD_WIDTH - 1 || y == FIELD_HEIGHT - 1 {
+                9
+            } else {
+                0
+            }
+        }
+    }
+    
+    // let mut screen_buffer: &mut [u8] = &mut [65; 144]; // look into Cursors; that seek function is looking might useful.
+    let mut stdout = stdout();
+    
+    stdout.write(b"\x1B[2J");
+    
+    for x in 0..FIELD_WIDTH {
+        for y in 0..FIELD_HEIGHT {
+            stdout.write(
+                &[" ABCDEFG=#".as_bytes()[p_field[(y * FIELD_WIDTH + x) as usize] as usize]]
+            )?;
+        }
+        stdout.write("\n".as_bytes()).unwrap();
+    }
+    stdout.write(b"\x1B[0;0H");
+    stdout.write(b"That's all folks!\n");
+    
+    // stdout.write(screen_buffer)?;
+    stdout.flush()?;    
+    Ok(())
+}
+    
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 //based off of Javidx9's tetris tutorial in C++. (https://youtu.be/8OK8_tHeCIA)
 
 use std::io::{Write, stdout};
-use crossterm::{ExecutableCommand, Result, terminal, cursor};
+use crossterm::{ExecutableCommand, Result, terminal, event::{read, poll, Event, KeyCode}, cursor};
 use std::{thread, time};
 
 mod experiment;
 
-const FIELD_WIDTH: usize = 12;
-const FIELD_HEIGHT: usize = 18;
+const FIELD_WIDTH: u8 = 12;
+const FIELD_HEIGHT: u8 = 18;
 
 fn main() -> Result<()> {
-    
-
     
     // let mut tetromino: [String; 7] = [
     //     String::from(), 
@@ -24,90 +22,117 @@ fn main() -> Result<()> {
     // ];
     
 
-    //leave this as a char (or u8), just convert to a byte it's time to write?
-    let mut tetromino: [&str; 7] = [
-        "..X...X...X...X.", 
-        "..X..XX...X.....", 
-        ".....XX..XX.....", 
-        "..X..XX..X......", 
-        ".X...XX...X.....", 
-        ".X...X...XX.....", 
-        "..X...X..XX....." 
-    ];
+    // // leave this as a char (or u8), just convert to a byte it's time to write?
+    // let tetromino: [&str; 7] = [
+    //     "..X...X...X...X.", 
+    //     "..X..XX...X.....", 
+    //     ".....XX..XX.....", 
+    //     "..X..XX..X......", 
+    //     ".X...XX...X.....", 
+    //     ".X...X...XX.....", 
+    //     "..X...X..XX....." 
+    // ];
 
-    let mut p_field: [usize; FIELD_WIDTH * FIELD_HEIGHT] = [0; FIELD_WIDTH * FIELD_HEIGHT];
+    // let mut p_field: [u8; (FIELD_WIDTH * FIELD_HEIGHT) as usize] = [0; (FIELD_WIDTH * FIELD_HEIGHT) as usize];
 
-    for x in 0..FIELD_WIDTH {
-        for y in 0..FIELD_HEIGHT {
-            p_field[y * FIELD_WIDTH + x] = 
+    // for x in 0..FIELD_WIDTH {
+    //     for y in 0..FIELD_HEIGHT {
+    //         p_field[(y * FIELD_WIDTH + x) as usize] = 
             
-            if x == 0 || x == FIELD_WIDTH - 1 || y == FIELD_HEIGHT - 1 {
-                //9 is the border in Javid's example.
-                9
-            }
+    //         if x == 0 || x == FIELD_WIDTH - 1 || y == FIELD_HEIGHT - 1 {
+    //             //9 is the border in Javid's example.
+    //             9
+    //         }
     
-            else {
-                0
-            }
-        }
-    };
+    //         else {
+    //             0
+    //         }
+    //     }
+    // };
 
-    let mut buffer = stdout();
+    // let mut buffer = stdout();
+    // buffer.execute(terminal::Clear(terminal::ClearType::All))?;           
+
+    // terminal::enable_raw_mode().unwrap();
+
+    // let mut current_piece = 0;
+    // let mut current_rotation = 0;
+    // let mut current_x = FIELD_WIDTH as u8 / 2;
+    // let mut current_y = 0;
     
-    buffer.execute(terminal::Clear(terminal::ClearType::All))?;
-    buffer.execute(terminal::SetSize(terminal::size()?.0, terminal::size()?.1))?;            
-
-    let current_piece = 2;
-    let current_rotation = 0;
-    let current_x = FIELD_WIDTH as u8 / 2;
-    let current_y = 0;
+    // let mut key: [bool; 4] = [false; 4];
     
-    let game_over = false;
+    // let game_over = false;
 
-    while game_over == false {
+    // while game_over == false {
 
-    // timing
+    // // timing
+    // thread::sleep(time::Duration::from_millis(50));
+    // // input
+    // // for _ in 0..4 {
+    //     // switch with non-blocking function.
+    //     if poll(time::Duration::from_millis(0))? {
+    //         match read()? {
+    //             Event::Key(pressed_key) => 
+    //                 match pressed_key.code {
+    //                     KeyCode::Left => key[0] = true,
+    //                     KeyCode::Down => key[1] = true,
+    //                     KeyCode::Right => key[2] = true,
+    //                     KeyCode::Char('z') =>key[3] = true,
+    //                     _ => (),
+    //                 },
     
-    // input
+    //             _ => (),
+    //         }
+    //     }
+    // // }
+    // // logic
+    // if key[0] && does_tetromino_fit(tetromino, p_field, current_rotation, current_piece, current_x - 1, current_y) { current_x -= 1; key[0] = false }
+    // if key[1] && does_tetromino_fit(tetromino, p_field, current_rotation, current_piece, current_x + 1, current_y) { current_y += 1; key[1] = false }
+    // if key[2] && does_tetromino_fit(tetromino, p_field, current_rotation, current_piece, current_x, current_y + 1) { current_x += 1; key[2] = false }
+    // // if key[3] && does_tetromino_fit() { current_x += 1 }
     
-    // logic
+    // // output
 
-    // output
-
-        // draw field
-        for x in 0..FIELD_WIDTH {
-            for y in 0..FIELD_HEIGHT {
+    //     // draw field
+    //     for x in 0..FIELD_WIDTH {
+    //         for y in 0..FIELD_HEIGHT {
     
-                buffer.execute(cursor::MoveTo((x) as u16, (y) as u16))?;
-                buffer.write(
-                    // this is an expression that returns a reference to a slice of u8, which indexed by an indexed value of p_field.
-                    &[" ABCDEFG=#".as_bytes()[p_field[y * FIELD_WIDTH + x]]]
-                );
+    //             buffer.execute(cursor::MoveTo(x as u16, y as u16))?;
+    //             buffer.write(
+    //                 // this is an expression that returns a reference to a slice of u8, which indexed by an indexed value of p_field.
+    //                 &[" ABCDEFG=#".as_bytes()[p_field[(y * FIELD_WIDTH + x) as usize] as usize]]
+    //             )?;
     
-                thread::sleep(time::Duration::from_millis(50));
-            }
-        }
+    //             // thread::sleep(time::Duration::from_millis(50));
+    //         }
+    //     }
 
-        // draw current piece
-        for px in 0..4 {
-            for py in 0..4 {
-                if tetromino[current_piece as usize].as_bytes()[rotate(px, py, current_rotation) as usize] == b'X' {
-                    buffer.execute(cursor::MoveTo((current_y + px) as u16, (current_x + py) as u16))?;
-                    buffer.write(&[current_piece + 65]);
-                }
-            }
-        }
-    }
-    println!("Terminal size: {:?}", terminal::size()?);
-
-    println!("{:?}", tetromino[0usize].chars().nth(0).unwrap());
-
+    //     // draw current piece
+    //     for px in 0..4 {
+    //         for py in 0..4 {
+    //             if tetromino[current_piece as usize].as_bytes()[rotate(px, py, current_rotation) as usize] == b'X' {
+    //                 buffer.execute(cursor::MoveTo((current_x + px) as u16, (current_y + py) as u16))?;
+    //                 buffer.write(&[current_piece + 65])?;
+    //             }
+    //         }
+    //     }
+    // } // end of game loop
+    // buffer.execute(cursor::MoveTo(terminal::size()?.0, terminal::size()?.1))?;
+    // println!("\nTerminal size: {:?}", terminal::size()?);
+    
+    // println!("{:?}", tetromino[0usize].chars().nth(0).unwrap());
+    
+    // experiment::alternate_screen()?;
+    // experiment::keyboard_events()?;
+    experiment::no_library_extended()?;
+    // buffer.flush();
     Ok(())
     
 }
 
 // remember to pass tetro and p_field!!!!
-fn does_tetromino_fit (tetro: [&str; 7], p_field: [usize; FIELD_WIDTH & FIELD_HEIGHT], id_tetromino: u8, rotation: u8, x_pos: u8, y_pos: u8) -> bool {
+fn _does_tetromino_fit (tetro: [&str; 7], p_field: [u8; (FIELD_WIDTH * FIELD_HEIGHT) as usize], id_tetromino: u8, rotation: u8, x_pos: u8, y_pos: u8) -> bool {
     for px in 0..4 {
         for py in 0..4 {
             // gets index into piece
